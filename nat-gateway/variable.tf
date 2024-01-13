5b02c0f0bb623731599096abeb5981db938e174a
LSanti94 <luissantillan1994@hotmail.com>
2024-01-13 00:01:44 -0500

    Create nat gateway module

---
+++ nat-gateway/variable.tf
@@ -0,0 +1,13 @@
+# nat-gateway variables
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "internet_gateway" {}
+variable "vpc_id" {}
+variable "private_route_table_az1_cidr" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_route_table_az2_cidr" {}
+variable "private_app_subnet_az2_id" {}
+variable "private_data_subnet_az2_id" {}
