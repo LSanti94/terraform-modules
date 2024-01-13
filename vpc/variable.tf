0e823522cdd7dd3d8f16be8c26d83123a643c19f
LSanti94 <luissantillan1994@hotmail.com>
2024-01-12 22:03:34 -0500

    Create vpc module

---
+++ vpc/variable.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {}
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "public_route_table_cidr"{}
+variable "private_app_subnet_az1_cidr"{}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr"{}
+variable "private_data_subnet_az2_cidr"{}
