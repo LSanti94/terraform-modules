51b2d744f4a39507e9f4ddd4a61b0faa4dbc7f24
LSanti94 <luissantillan1994@hotmail.com>
2024-01-16 16:07:30 -0500

    Create iam role

--- iam-role/main.tf
+++ iam-role/main.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.bucket_name}-${var.env_file_bucket_name}/*"
+      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/*"
     ]
   }
 
@@ -41,20 +41,20 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.bucket_name}-${var.env_file_bucket_name}"
+      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}"
     ]
   }
 }
 
 # create iam policy
 resource "aws_iam_policy" "ecs_task_execution_policy" {
-  name   = "${var.project_name_bucket}-${var.environment}-ecs-task-execution-role-policy"
+  name   = "${var.project_name}-${var.environment}-ecs-task-execution-role-policy"
   policy = data.aws_iam_policy_document.ecs_task_execution_policy_document.json
 }
 
 # create an iam role
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "${var.project_name_bucket}-${var.environment}-ecs-task-execution-role"
+  name               = "${var.project_name}-${var.environment}-ecs-task-execution-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_policy_document.json
 }
 
--- iam-role/variable.tf
+++ iam-role/variable.tf
@@ -1,3 +1,3 @@
-variable "project_name" {}
+variable "env_file_bucket_name" {}
 variable "environment" {}
-variable "bucket_name" {}
+variable "project_name" {}
