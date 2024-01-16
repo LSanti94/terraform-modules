515c0219c6d86fb847db4d1a4a01d34314e41aa4
LSanti94 <luissantillan1994@hotmail.com>
2024-01-16 16:05:18 -0500

    Create s3 module

--- iam-role/variable.tf
+++ iam-role/variable.tf
@@ -1,4 +1,3 @@
-variable "project_name_bucket" {}
-variable "env_file_bucket_name" {}
+variable "project_name" {}
 variable "environment" {}
 variable "bucket_name" {}
--- s3/variable.tf
+++ s3/variable.tf
@@ -1,3 +1,3 @@
-variable "project_name_bucket" {}
+variable "project_name" {}
 variable "env_file_bucket_name" {}
 variable "env_file_name" {}
